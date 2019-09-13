e6a94876089283402c1de36ce1c2689158d97ab3
kimdongwon <ehddnjs8989@gmail.com>
2019-09-13 11:49:02 +0900

    add redux configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-import { CookiesProvider } from 'react-cookie';
+import { CookiesProvider } from "react-cookie";
+import { Provider } from "react-redux";
+import configureStore from "./stores/configureStore";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <CookiesProvider>
-    <App />
-  </CookiesProvider>,
-  document.getElementById('root'),
+  <Provider store={store}>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
