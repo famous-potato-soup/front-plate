89345f4011b9cca78a3081550178a8cf6186e7eb
super-fishz <super_fishz@core-a.org>
2019-09-15 21:21:22 +0900

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# 소문난 감자탕 앞접시
-
-[![CircleCI](https://circleci.com/gh/famous-potato-soup/front-plate.svg?style=shield)](https://circleci.com/gh/famous-potato-soup/front-plate)
-[![CodeCov](https://codecov.io/gh/famous-potato-soup/front-plate/branch/master/graph/badge.svg)](https://codecov.io/gh/famous-potato-soup/front-plate)
---
+++ precache-manifest.8e221ff8544fe746611bbb4e306f8344.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbe37a7bc07e7d1174c2826f604848a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79902e29bdd70673a2f8",
+    "url": "/static/css/main.b2a9db5a.chunk.css"
+  },
+  {
+    "revision": "e76feff67b6f543973cc",
+    "url": "/static/js/2.30e1410f.chunk.js"
+  },
+  {
+    "revision": "79902e29bdd70673a2f8",
+    "url": "/static/js/main.79892ef1.chunk.js"
+  },
+  {
+    "revision": "dc2c2b6b5cd60b79df59",
+    "url": "/static/js/runtime~main.5bc73931.js"
+  }
+]);
