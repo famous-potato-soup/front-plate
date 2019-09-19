b5a6d3f040f8703ce069a4ca537d4f4ffc216564
jeongukjae <JeongUkJae@gmail.com>
2019-09-19 21:40:27 +0900

    ready component와 이벤트 연결 & board 마운트

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { Board } from './board';
 
 import { withCookies, useCookies } from 'react-cookie';
 
 import LoginComponent from './tsx/OAuthLogin';
 import ReadyComponent from './tsx/ReadyComponent';
-import { WinComponent, LoseComponent } from './tsx/Result';
+import { Board } from './board';
 
 const App: React.FC = () => {
+  const [isGaming, setIsGaming] = useState<boolean>(false);
   const [cookie] = useCookies(['user']);
+
+  const handleGameStart = () => {
+    setIsGaming(true);
+  };
+
   return (
     <div className="App">
-      <div className="content">{cookie.user ? <ReadyComponent /> : <LoginComponent />}</div>
+      <div className="content">
+        {cookie.user ? isGaming ? <Board /> : <ReadyComponent onGameStart={handleGameStart} /> : <LoginComponent />}
+      </div>
       {/* <WinComponent /> */}
     </div>
   );
